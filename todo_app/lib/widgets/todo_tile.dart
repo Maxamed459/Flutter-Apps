e93a261b5766a4d6ead79338810300c90ecf4dbb
Maxamed459 <maxamedmahdi459@gmail.com>
2026-06-27 20:28:42 +0300

    refactor TodoTile to implement Slidable for task deletion functionality

--- todo_app/lib/widgets/todo_tile.dart
+++ todo_app/lib/widgets/todo_tile.dart
@@ -1,36 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class TodoTile extends StatelessWidget {
   final String taskTitle;
   final bool taskCompleted;
   Function(bool?)? onChanged;
+  Function(BuildContext)? onDelete;
 
   TodoTile({
     super.key,
     required this.taskTitle,
     required this.taskCompleted,
     required this.onChanged,
+    required this.onDelete,
   });
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 26, vertical: 10),
-      child: Container(
-        padding: EdgeInsets.all(20),
-        decoration: BoxDecoration(
-          color: Colors.blue,
-          borderRadius: BorderRadius.circular(10),
-        ),
-        child: Row(
+      child: Slidable(
+        endActionPane: ActionPane(
+          motion: StretchMotion(),
           children: [
-            Checkbox(value: taskCompleted, onChanged: onChanged),
-
-            Text(
-              taskTitle,
-              style: TextStyle(fontSize: 22, color: Colors.white),
+            SlidableAction(
+              onPressed: onDelete,
+              icon: Icons.delete,
+              backgroundColor: Colors.red.shade300,
+              borderRadius: BorderRadius.circular(10),
             ),
           ],
+        ),
+        child: Container(
+          padding: EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            color: Colors.blue,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Row(
+            children: [
+              Checkbox(
+                value: taskCompleted,
+                onChanged: onChanged,
+                activeColor: Colors.black,
+              ),
+
+              Expanded(
+                child: Text(
+                  taskTitle,
+                  softWrap: true,
+                  style: TextStyle(
+                    color: Colors.black,
+                    decoration: taskCompleted
+                        ? TextDecoration.lineThrough
+                        : TextDecoration.none,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
