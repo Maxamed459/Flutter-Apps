3e779604d13f61ad3821da3911478505d852a969
Maxamed459 <maxamedmahdi459@gmail.com>
2026-06-27 20:28:23 +0300

    refactor HomePage to use StatefulWidget and implement task management features

--- todo_app/lib/pages/home_page.dart
+++ todo_app/lib/pages/home_page.dart
@@ -1,8 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/widgets/dialog_box.dart';
 import 'package:todo_app/widgets/todo_tile.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final _controller = TextEditingController();
+
+  List tasks = [
+    ["Code 1 hour every day", false],
+    ["Sleep at 10:00PM", true],
+  ];
+
+  void checkboxChanged(bool? value, int index) {
+    setState(() {
+      tasks[index][1] = !tasks[index][1];
+    });
+  }
+
+  void createNewTAsk() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return DialogBox(
+          controller: _controller,
+          onSave: onSave,
+          oncancel: () => Navigator.of(context).pop(),
+        );
+      },
+    );
+  }
+
+  void onSave() {
+    setState(() {
+      tasks.add([_controller.text, false]);
+      _controller.clear();
+    });
+    Navigator.of(context).pop();
+  }
+
+  void deleteTask(int index) {
+    setState(() {
+      tasks.removeAt(index);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,25 +61,21 @@ class HomePage extends StatelessWidget {
           style: TextStyle(fontSize: 22, color: Colors.white),
         ),
       ),
-      body: ListView(
-        children: [
-          SizedBox(height: 20),
-          TodoTile(
-            taskTitle: "Make dinner",
-            taskCompleted: false,
-            onChanged: (p0) {},
-          ),
-          TodoTile(
-            taskTitle: "Make Tutorial",
-            taskCompleted: true,
-            onChanged: (p0) {},
-          ),
-          TodoTile(
-            taskTitle: "Sleep at 10:00PM",
-            taskCompleted: false,
-            onChanged: (p0) {},
-          ),
-        ],
+      body: ListView.builder(
+        itemCount: tasks.length,
+        itemBuilder: (context, index) {
+          return TodoTile(
+            taskTitle: tasks[index][0],
+            taskCompleted: tasks[index][1],
+            onChanged: (value) => checkboxChanged(value, index),
+            onDelete: (context) => deleteTask(index),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.blue,
+        onPressed: createNewTAsk,
+        child: Icon(Icons.add, size: 30, color: Colors.white),
       ),
     );
   }
